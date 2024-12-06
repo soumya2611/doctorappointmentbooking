f80c3d25277f6e688d8e60400b18d47feec59356
Soumya Ranjan Behera <159926859+soumya2611@users.noreply.github.com>
2024-12-06 11:22:25 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "₹";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL||'https://doctorappointmentbooking-adf6.onrender.com';
   const [token, setToken] = useState(
     localStorage.getItem("token") ? localStorage.getItem("token") : false
   );
